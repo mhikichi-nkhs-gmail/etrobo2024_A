4970d47891816660133d008af9d2a1c31a9f17e7
Hiratai0801 <nkhsha031001@g.neec.ac.jp>
2024-07-30 10:45:48 +0900

    smartキャリー

--- section/SmartSection.cpp
+++ section/SmartSection.cpp
@@ -8,19 +8,31 @@ SmartSection::SmartSection()
 
 bool SmartSection::run()
 {
-    
+    print("スマートきゃりーぱみゅぱみゅ")
+    switch(NO)
+    {
+    case SET_VIR_LINE:
+        set(VIR);
+        NO = END;
+        break;
+    case END:
+        printf("END\n");
+        return true;
+    }
+    return false;
 }
 
 void DoubleSection::course(int direct)
 {
     if(direct == 0)
     {
-        NO = SET_LBLUE_MARKER;
+        NO = SET_VIR_LINE;
         num1 = 0;
     }
     else
     {
-        NO = SET_RBLUE_MARKER;
+        NO = SET_VIR_LINE;
         num1 = 4;
     }
 }
+
