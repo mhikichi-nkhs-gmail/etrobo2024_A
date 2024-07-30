05ffc7312cd6ec232ab531dc983bdabbbbfb40fa
Hiratai0801 <nkhsha031001@g.neec.ac.jp>
2024-07-30 10:07:57 +0900

    Update SmartSection.cpp

--- section/SmartSection.cpp
+++ section/SmartSection.cpp
@@ -10,3 +10,17 @@ bool SmartSection::run()
 {
     
 }
+
+void DoubleSection::course(int direct)
+{
+    if(direct == 0)
+    {
+        NO = SET_LBLUE_MARKER;
+        num1 = 0;
+    }
+    else
+    {
+        NO = SET_RBLUE_MARKER;
+        num1 = 4;
+    }
+}
